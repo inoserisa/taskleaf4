d21baa3b5c7e0d80355bed739ef4dd4feac6e421
inoserisa <inoserisa@inoserisanoMac-mini.local>
2024-03-21 15:58:54 +0900

    オブジェクト指向規約の適用/プリミティブ型・文字列型

---
+++ minitest/test.rb
@@ -0,0 +1,77 @@
+require 'minitest/autorun'
+require './object_oriented/vending_machine'
+require './object_oriented/drink_type'
+require './object_oriented/coin'
+
+class VendingMachineTest < Minitest::Test
+  def setup
+    @vm = VendingMachine.new
+  end
+
+  def test_500円でコーラを購入
+    drink = @vm.buy(Coin::FIVE_HUNDRED, DrinkType::COKE)
+    change = @vm.refund
+
+    assert_equal(DrinkType::COKE, drink.kind)
+    assert_equal([Coin::ONE_HUNDRED] * 4, change)
+  end
+
+  def test_10円ではコーラは買えない
+    drink = @vm.buy(10, DrinkType::COKE)
+    change = @vm.refund
+
+    assert_nil(drink)
+    assert_equal([10], change)
+  end
+
+  def test_コーラの在庫が無いときにコーラは買えない
+    @vm.buy(Coin::ONE_HUNDRED, DrinkType::TEA)
+    @vm.buy(Coin::ONE_HUNDRED, DrinkType::TEA)
+    @vm.buy(Coin::ONE_HUNDRED, DrinkType::TEA)
+    @vm.buy(Coin::ONE_HUNDRED, DrinkType::TEA)
+    @vm.buy(Coin::ONE_HUNDRED, DrinkType::TEA)
+    drink = @vm.buy(Coin::ONE_HUNDRED, DrinkType::TEA)
+    change = @vm.refund
+
+    assert_nil(drink)
+    assert_equal([Coin::ONE_HUNDRED], change)
+  end
+
+  def testダイエットコーラの在庫が無いときにダイエットコーラは買えない
+    @vm.buy(Coin::ONE_HUNDRED, DrinkType::DIET_COKE)
+    @vm.buy(Coin::ONE_HUNDRED, DrinkType::DIET_COKE)
+    @vm.buy(Coin::ONE_HUNDRED, DrinkType::DIET_COKE)
+    @vm.buy(Coin::ONE_HUNDRED, DrinkType::DIET_COKE)
+    @vm.buy(Coin::ONE_HUNDRED, DrinkType::DIET_COKE)
+    drink = @vm.buy(Coin::ONE_HUNDRED, DrinkType::DIET_COKE)
+    change = @vm.refund
+
+    assert_nil(drink)
+    assert_equal([Coin::ONE_HUNDRED], change)
+  end
+
+  def testお茶の在庫が無いときにお茶は買えない
+    @vm.buy(Coin::ONE_HUNDRED, DrinkType::TEA)
+    @vm.buy(Coin::ONE_HUNDRED, DrinkType::TEA)
+    @vm.buy(Coin::ONE_HUNDRED, DrinkType::TEA)
+    @vm.buy(Coin::ONE_HUNDRED, DrinkType::TEA)
+    @vm.buy(Coin::ONE_HUNDRED, DrinkType::TEA)
+    drink = @vm.buy(Coin::ONE_HUNDRED, DrinkType::TEA)
+    change = @vm.refund
+
+    assert_nil(drink)
+    assert_equal([Coin::ONE_HUNDRED], change)
+  end
+
+  def test_釣り銭が足りないときにコーラは買えない
+    @vm.buy(Coin::FIVE_HUNDRED, DrinkType::COKE)
+    @vm.refund
+    @vm.buy(Coin::FIVE_HUNDRED, DrinkType::COKE)
+    @vm.refund
+    drink = @vm.buy(Coin::FIVE_HUNDRED, DrinkType::COKE)
+    change = @vm.refund
+
+    assert_nil(drink)
+    assert_equal([Coin::FIVE_HUNDRED], change)
+  end
+end
